98b467d200c0e80985d47ca1bf41b4fa6c8322a9
BSonnemberg <up202206098@up.pt>
2023-12-18 17:51:03 +0000

    graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,1 +1,157 @@
 #include "Graph.h"
+
+template <class T>
+Vertex<T>::Vertex(T in): info(in) {}
+
+template <class T>
+Edge<T>::Edge(Vertex<T> *d, double d1): dest(d), distance(d1) {}
+
+
+template <class T>
+int Graph<T>::getNumVertex() const {
+    return vertexSet.size();
+}
+
+template <class T>
+vector<Vertex<T> * > Graph<T>::getVertexSet() const {
+    return vertexSet;
+}
+
+template<class T>
+T Vertex<T>::getInfo() const {
+    return info;
+}
+
+template<class T>
+void Vertex<T>::setInfo(T in) {
+    Vertex::info = in;
+}
+
+
+template<class T>
+Vertex<T> *Edge<T>::getDest() const {
+    return dest;
+}
+
+template<class T>
+void Edge<T>::setDest(Vertex<T> *d) {
+    Edge::dest = d;
+}
+
+template<class T>
+double Edge<T>::getDistance() const {
+    return distance;
+}
+
+template<class T>
+void Edge<T>::setDistance(double weight) {
+    Edge::distance = distance;
+}
+
+/*
+ * Auxiliary function to find a vertex with a given content.
+ */
+template <class T>
+Vertex<T> * Graph<T>::findVertex(const T &in) const {
+    for (auto v : vertexSet)
+        if (v->info == in)
+            return v;
+    return NULL;
+}
+
+template <class T>
+bool Vertex<T>::isVisited() const {
+    return visited;
+}
+
+template <class T>
+void Vertex<T>::setVisited(bool v) {
+    Vertex::visited = v;
+}
+
+template<class T>
+const vector<Edge<T>> &Vertex<T>::getAdj() const {
+    return adj;
+}
+
+template <class T>
+void Vertex<T>::setAdj(const vector<Edge<T>> &adj) {
+    Vertex::adj = adj;
+}
+
+template <class T>
+bool Graph<T>::addVertex(const T &in) {
+    if (findVertex(in) == NULL) {
+        Vertex<T> * it = new Vertex<T> (in);
+        vertexSet.push_back(it);
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+bool Graph<T>::addEdge(const T &sourc, const T &dest, double w) {
+    if (findVertex(sourc) != NULL && findVertex(dest) != NULL) {
+        findVertex(sourc)->addEdge(findVertex(dest),w);
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+void Vertex<T>::addEdge(Vertex<T> *d, double w) {
+    this->adj.push_back(Edge<T>(d,w));
+}
+
+
+template <class T>
+bool Graph<T>::removeEdge(const T &sourc, const T &dest) {
+    vector<Edge<T>> aux= findVertex(sourc)->getAdj();
+    for(auto it:aux){
+        if(it.getDest()== findVertex(dest)){
+            findVertex(sourc)->removeEdgeTo(findVertex(dest));
+            return true;
+        }
+    }
+    return false;
+}
+
+
+template <class T>
+bool Vertex<T>::removeEdgeTo(Vertex<T> *d) {
+    auto it=this->adj.begin();
+    while(it!=this->adj.end()){
+        if(it->getDest()==d){
+            this->adj.erase(it);
+            return true;
+        }
+        it++;
+    }
+    return false;
+}
+
+
+template <class T>
+bool Graph<T>::removeVertex(const T &in) {
+    auto it=vertexSet.begin();
+    Vertex<T> *foundVertex= nullptr;
+    while(it!=vertexSet.end()){
+        if((*it)->getInfo()==in){
+            foundVertex=*it;
+            vertexSet.erase(it);
+            break;
+        }
+        else{
+            it++;
+        }
+    }
+    if(foundVertex==NULL){
+        return false;
+    }
+    for(auto it:vertexSet){
+        if(it==foundVertex){
+            it->removeEdgeTo(foundVertex);
+        }
+    }
+    return true;
+}
--- Graph.h
+++ Graph.h
@@ -1,12 +1,70 @@
 #ifndef PROJETO2_AED_GRAPH_H
 #define PROJETO2_AED_GRAPH_H
+
+#include "Airlines.h"
+#include "Airports.h"
+#include <string>
+#include <set>
+#include <list>
+
 using namespace std;
 
+template <class T> class Edge;
+template <class T> class Graph;
+template <class T> class Vertex;
 
-class Graph {
+template <class T>
+class Vertex {
+    T info;
+    vector<Edge<T>> adj;
+    bool visited;
+
+    void addEdge(Vertex<T> *dest, double distance);
+    bool removeEdgeTo(Vertex<T> *d);
+
 public:
+    Vertex(T in);
+    T getInfo() const;
+    void setInfo(T in);
+    bool isVisited() const;
+    void setVisited(bool v);
+    const vector<Edge<T>> &getAdj() const;
+    void setAdj(const vector<Edge<T>> &adj);
+    friend class Graph<T>;
+};
+
+template <class T>
+class Edge {
+    Vertex<T> * dest;
+    double distance;
+public:
+    Edge(Vertex<T> *d, double w);
+    Vertex<T> *getDest() const;
+    void setDest(Vertex<T> *dest);
+    double getDistance() const;
+    void setDistance(double distance);
+    friend class Graph<T>;
+    friend class Vertex<T>;
+};
+
+template <class T>
+class Graph {
+    vector<Vertex<T> *> vertexSet;
+public:
+    Vertex<T> *findVertex(const T &in) const;
+    int getNumVertex() const;
+    bool addVertex(const T &in);
+    bool removeVertex(const T &in);
+    bool addEdge(const T &sourc, const T &dest, double w);
+    bool removeEdge(const T &sourc, const T &dest);
+    vector<Vertex<T> * > getVertexSet() const;
+    int inDegree(const T &v) const;
+};
+
+
 
 };
+
 
 
 #endif //PROJETO2_AED_GRAPH_H
